974cb6d2e875dbe317a17c5c72090d57ffc43cb3
liqint-web <liqint@kean.edu>
2026-02-06 18:52:22 -0500

    Add files via upload

---
+++ MyAvgLib/Average.cs
@@ -0,0 +1,14 @@
+﻿namespace MyAvgLib;
+
+public class Average
+{
+    public static double Avg(double a, double b)
+    {
+        return (a + b) / 2.0;
+    }
+
+    public static double Avg(double a, double b, double c)
+    {
+        return (a + b + c) / 3.0;
+    }
+}
